4749daa8b0af04a4f901c825217c902220371c00
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2021-03-23 11:53:33 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 // Set up Canvas and Context
 let cnv = document.getElementById("my-canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 800;
-cnv.height = 600;
+cnv.width = 768;
+cnv.height = 528;
 
 // HTML Variables
 let mapImgEl = document.getElementById("map");
+
+// Global Variables
+let roomRow = 4;
+let roomCol = 3;
+
+let redStairs = {
+  row: 0,
+  col: 0
+};
+let redBush = {
+  row: 2,
+  col: 3
+}
+
+let tileGrid = [];
+for (let y = 1; y <= 120; y += 17) {
+  let tempRow = [];
+  for (let x = 1; x <= 290; x += 17) {
+    tempRow.push({
+      x: x,
+      y: y
+    });
+  }
+  tileGrid.push(tempRow);
+}
+
 
 
 // Start Draw Function once all images load...
@@ -13,10 +39,35 @@ window.addEventListener("load", draw);
 
 // Draw Function (Main Program Loop)
 function draw() {
-  console.log("hello");
 
-  ctx.drawImage(mapImgEl, 200, 0, 200, 200, 0, 0, 800, 600);
+  let coords = tileGrid[redBush.row][redBush.col];
+  ctx.drawImage(mapImgEl, coords.x, coords.y, 16, 16, 0, 0, 48, 48);
 
   // Call draw again to loop program
   requestAnimationFrame(draw);
 }
+
+// (1, 1) // [0,0]
+// (18, 1) // [0,1]
+// (35, 1) // [0,2]
+// (52, 1) // [0,3]
+// ...
+// (290, 1) // [0, 17]
+
+// (1, 18) // [1,0]
+// (18, 18) // [1,1]
+// (35, 18) // [1,2]
+// (52, 18) // [1,3]
+// ...
+// (290, 18) // [1, 17]
+
+// (1, 35) // [2,0]
+// (18,35) // [2,1]
+// (35,35) // [2,2]
+// (52,35) // [2,3]
+// ...
+// (290, 35) // [2, 17]
+
+// ... 
+
+// ... (290, 120)
