39cd8f4e5292f5e5e469b6cd2b918acd95994f61
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2021-03-24 15:07:28 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,18 @@
+// LEGEND OF ZELDA
+
+// Constants
+let ROOM_ROWS = 11;
+let ROOM_COLS = 16;
+let TILE_SIZE = 48;
+
 // Set up Canvas and Context
 let cnv = document.getElementById("my-canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 768;
-cnv.height = 528;
+cnv.width = ROOM_COLS * TILE_SIZE;
+cnv.height = ROOM_ROWS * TILE_SIZE;
 
 // HTML Variables
 let mapImgEl = document.getElementById("map");
-
-// Global Variables
-let roomRow = 4;
-let roomCol = 3;
-
-let redStairs = {
-  row: 0,
-  col: 0
-};
-let redBush = {
-  row: 2,
-  col: 3
-}
-
-let tileGrid = [];
-for (let y = 1; y <= 120; y += 17) {
-  let tempRow = [];
-  for (let x = 1; x <= 290; x += 17) {
-    tempRow.push({
-      x: x,
-      y: y
-    });
-  }
-  tileGrid.push(tempRow);
-}
-
-
 
 // Start Draw Function once all images load...
 window.addEventListener("load", draw);
@@ -40,8 +20,14 @@ window.addEventListener("load", draw);
 // Draw Function (Main Program Loop)
 function draw() {
 
-  let coords = tileGrid[redBush.row][redBush.col];
-  ctx.drawImage(mapImgEl, coords.x, coords.y, 16, 16, 0, 0, 48, 48);
+  for (let row = 0; row < 11; row++) {
+    for (let col = 0; col < 16; col++) {
+      let tileNum = room1[row][col];
+      let coords = tileClipCoords[tileNum];
+      ctx.drawImage(mapImgEl, coords.x, coords.y, 16, 16, col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+  }
+
 
   // Call draw again to loop program
   requestAnimationFrame(draw);
